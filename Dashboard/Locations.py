8b457ee397234ad65d48d01534e9f57111368ba3
TomLeppens99 <124497697+TomLeppens99@users.noreply.github.com>
2023-06-02 16:36:28 +0200

    Add files via upload

--- Dashboard/Locations.py
+++ Dashboard/Locations.py
@@ -1,5 +1,94 @@
 import pandas as pd
 import plotly.express as px
+import pandas as pd
+from collections import defaultdict
+
+def read_popular_times_histogram(file_name):
+    df = pd.read_json(file_name)
+    df = df[['popularTimesHistogram']]
+    df.columns = ['popularTimesHistogram']
+    return df
+
+His_Hairs_data = read_popular_times_histogram('His en hairs.json')
+filosofia = read_popular_times_histogram('filosifia.json')
+Maxim = read_popular_times_histogram('MAXIM occupancy.json')
+Taste = read_popular_times_histogram('Taste.json')
+Xior = read_popular_times_histogram('Xior.json')
+kapel = read_popular_times_histogram('kapel.json')
+vrijthof = read_popular_times_histogram('vrijthof.json')
+naamsestraat = read_popular_times_histogram('Naamsestraat81.json')
+
+
+def aggregate_histogram(df):
+    combined_data = defaultdict(list)
+
+    for index, row in df.iterrows():
+        histogram = row['popularTimesHistogram']
+        if pd.isna(histogram):
+            continue
+        for day, hours in histogram.items():
+            for hour_info in hours:
+                hour = hour_info['hour']
+                occupancy = hour_info['occupancyPercent']
+                # Append a new dictionary for each hour
+                combined_data[day].append({'hour': hour, 'occupancyPercent': occupancy})
+
+    # Convert the defaultdict to a regular dict
+    combined_data = dict(combined_data)
+
+    # Renaming the days
+    day_mapping = {
+        'Mo': 'Monday',
+        'Tu': 'Tuesday',
+        'We': 'Wednesday',
+        'Th': 'Thursday',
+        'Fr': 'Friday',
+        'Sa': 'Saturday',
+        'Su': 'Sunday'
+    }
+
+    combined_data = {day_mapping.get(k, k): v for k, v in combined_data.items()}
+
+    return combined_data
+
+
+    # First, aggregate the histograms
+Xior_pop = aggregate_histogram(Xior)
+Taste_pop = aggregate_histogram(Taste)
+Maxim_pop = aggregate_histogram(Maxim)
+filosofia_pop = aggregate_histogram(filosofia)
+His_Hairs_pop = aggregate_histogram(His_Hairs_data)
+kapel_pop = aggregate_histogram(kapel)
+vrijthof_pop = aggregate_histogram(vrijthof)
+naamsestraat_pop = aggregate_histogram(naamsestraat)
+
+# Then, create a new DataFrame
+df_pop = pd.DataFrame()
+
+# Add each histogram as a new column
+df_pop['Xior'] = [Xior_pop]
+df_pop['Taste'] = [Taste_pop]
+df_pop['Maxim'] = [Maxim_pop]
+df_pop['filosofia'] = [filosofia_pop]
+df_pop['His_Hairs'] = [His_Hairs_pop]
+df_pop['kapel'] = [kapel_pop]
+df_pop['vrijthof'] = [vrijthof_pop]
+df_pop['naamsestraat'] = [naamsestraat_pop]
+
+# Create a list of dictionaries for the data
+data = [
+    {'id': 6, 'name': 'Xior Studenthousing', 'popularTimesHistogram': Xior_pop},
+    {'id': 1, 'name': 'Taste', 'popularTimesHistogram': Taste_pop},
+    {'id': 5, 'name': 'Maxim', 'popularTimesHistogram': Maxim_pop},
+    {'id': 3, 'name': 'La Filosovia', 'popularTimesHistogram': filosofia_pop},
+    {'id': 8, 'name': 'His & Hears', 'popularTimesHistogram': His_Hairs_pop},
+    {'id': 2, 'name': 'Calvariekapel KU Leuven', 'popularTimesHistogram': kapel_pop},
+    {'id': 7, 'name': 'Historical Leuven Town hall', 'popularTimesHistogram': vrijthof_pop},
+    {'id': 4, 'name': 'Naamsestraat 81', 'popularTimesHistogram': naamsestraat_pop}
+]
+
+# Create the DataFrame
+df_pop = pd.DataFrame(data)
 
 # Read noise data
 noise_df = pd.read_csv('final_df.csv')
@@ -53,53 +142,11 @@ def dict_to_dataframe(data_dict, type_str):
 # Rename noise dataframe descritpion column to name and add type columm
 noise_df.rename(columns={'description': 'name'}, inplace=True)
 
+events_data_df = pd.read_excel('typedata.xlsx')
+predicted = pd.read_csv('noise_predictions.csv')
 
-google_data = pd.read_json('data_google_2.json')
-google_data.drop(['imagesCount', 'webResults', 'reviews', 'reviewsTags', 'orderBy', 
-         'questionsAndAnswers', 'updatesFromCustomers', 'reserveTableUrl',
-           'placesTags', 'subTitle','description','price','menu',
-          'locatedIn'	,'neighborhood'	,'street','city','postalCode','state','countryCode',
-          'plusCode',	'website',	'phone',	'temporarilyClosed',	'claimThisBusiness',
-          'permanentlyClosed',	'totalScore',	'placeId',	'categories',	'cid',	'url',	'scrapedAt',
-          	'popularTimesLiveText',	'popularTimesLivePercent',
-            'peopleAlsoSearch',	'additionalInfo',	'reviewsCount',	'reviewsDistribution', 'address' 	
-          ], axis=1, inplace=True)
-#google_data['type']= google_data['categoryName']
-google_data['name']= google_data['title']
-google_data.drop(['categoryName','title'], axis=1, inplace=True)
-#google_data['type'] = google_data['type'].replace({'Brunch restaurant': 'Restaurant',
-                                 #'Italian restaurant': 'Restaurant',
-                                 #'Vegan restaurant': 'Restaurant',
-                                 #'Turkish restaurant': 'Restaurant',
-                                 #'Sushi restaurant': 'Restaurant',
-                                 #'Fine dining restaurant': 'Restaurant',
-                                 #'Egyptian restaurant': 'Restaurant',
-                                 #'Fast food restaurant': 'Restaurant',
-                                 #'Cafe': 'Bar',})
-#google_data['latitude'] = google_data['location'].apply(lambda x: x['lat'])
-#google_data['longitude'] = google_data['location'].apply(lambda x: x['lng'])
-google_data.drop(['location'], axis=1, inplace=True)
-
-
-# Dictionary to map the new names
-new_names_dict = {
-    'Taste': 'Taste',
-    'Calvariekapel': 'Calvariekapel KU Leuven',
-    'La Filosofia': 'La Filosovia',
-    'Maxim': 'Maxim',
-    'Xior Studenthousing': 'Xior Studenthousing',
-    'Historical Leuven Town hall': 'Historical Leuven Town hall',
-    'His & Hairs': 'His & Hears'
-}
-google_data['name'] = google_data['name'].map(new_names_dict)
-new_row = pd.DataFrame({'name': ['Naamsestraat 81']})
-google_data = pd.concat([google_data, new_row], ignore_index=True)
-
-# Merge google_data and noise_df on the 'name' column
-combined_df = pd.merge(google_data, noise_df, on='name', how='left')
-
+# Merge pop_data_df and noise_df on the 'name' column
+combined_df = pd.merge(noise_df, df_pop,on='name', how='left')
 # Set the 'type' column in combined_df to 'Noise Meter'
 combined_df['type'] = 'Noise Meter'
-
-events_data_df = pd.read_excel('typedata.xlsx')
 
